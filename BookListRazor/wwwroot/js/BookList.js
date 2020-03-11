9c51dfadfeb62bc9e3ac73ee0ee550254fad4243
bala <bala@teknomec.com>
2020-03-11 15:49:20 +0530

    RazorCompleted

--- BookListRazor/wwwroot/js/BookList.js
+++ BookListRazor/wwwroot/js/BookList.js
@@ -19,7 +19,7 @@ function loadDataTable() {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                        <a href="/BookList/Upsert?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a>
                         &nbsp;
@@ -36,4 +36,31 @@ function loadDataTable() {
         },
         "width": "100%"
     });
+}
+
+function Delete(url){
+swal({
+    title:"Are you sure?",
+    text:"Once deleted, you will not be able to recover",
+    icon:"warning",
+    buttons: true,
+    dangerMode:true
+}).then((willDelete)=>{
+if(willDelete){
+$.ajax({
+type:"DELETE",
+url:url,
+success:function(data){
+if(data.success){
+toastr.success(data.message);
+dataTable.ajax.reload();
+}
+else{
+toastr.error(data.message);
+}
+
+}
+});
+}
+});
 }
